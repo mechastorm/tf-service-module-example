406042152bde58a08d34713cfb3272c9575fe43c
Shih Oon Liong <shih.liong@samsung.com>
2018-05-08 13:28:10 -0700

    Set explicit outbound rule

--- security_group.tf
+++ security_group.tf
@@ -11,10 +11,15 @@ resource "aws_security_group" "server" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["${data.aws_vpc.selected.cidr_block}"]
   }
 }
+
+data "aws_vpc" "selected" {
+  id = "${var.vpc_id}"
+}
+
 
